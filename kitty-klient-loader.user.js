ad783b31a0cef311da1f55a74f5d54d5b1d3e9fa
Collin Marshall <collinmarshall@Collins-Air.localdomain>
2026-09-19 07:22:18 -0500

    Pin loader to 7.0.40 core

--- kitty-klient-loader.user.js
+++ kitty-klient-loader.user.js
@@ -2,7 +2,7 @@
 // @name         kitty klient
 // @author       Coder Guy
 // @credits       random4ik — bot script
-// @version      7.0.39
+// @version      7.0.40
 // @icon         https://cdn.discordapp.com/icons/1540876076224356437/ac27c0ce87c4c46b407ebca78e150aeb.webp?size=2048
 // @description  kitty klient — a MooMoo.io client with adaptive zoom, fast autoheal, gear automation, combat tools, predictive placement, visual markers, CC0 background music, manual quick builds, and a fully rebindable keyboard/mouse controls HUD.
 // @match        *://moomoo.io/*
@@ -17,7 +17,7 @@
 // @run-at       document-start
 // @namespace    thisisirrelevandandidontknowwhygreasyforkneedsthisanyways
 // @license      MIT
-// @require      https://cdn.jsdelivr.net/gh/CoderGuy508/kitty-klient-cdn@35fd03a9869fd1313dce7a1ec7adb77d1ec583bd/kitty-klient-core.user.js
+// @require      https://cdn.jsdelivr.net/gh/CoderGuy508/kitty-klient-cdn@7fd7546e8a9ef37ab8f68a0302e2e271b1759584/kitty-klient-core.user.js
 // ==/UserScript==
 
 void 0;
