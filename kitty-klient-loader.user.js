1a6c82ed7b75768f4f68be68b8d06745b8b6ea2e
Collin Marshall <collinmarshall@Collins-MacBook-Air.local>
2026-09-20 21:20:40 -0500

    Publish Kitty 7.0.43 update metadata

--- kitty-klient-loader.user.js
+++ kitty-klient-loader.user.js
@@ -2,7 +2,7 @@
 // @name         kitty klient
 // @author       Coder Guy
 // @credits       random4ik — bot script
-// @version      7.0.42
+// @version      7.0.43
 // @icon         https://cdn.discordapp.com/icons/1540876076224356437/ac27c0ce87c4c46b407ebca78e150aeb.webp?size=2048
 // @description  kitty klient — a MooMoo.io client with adaptive zoom, fast autoheal, gear automation, combat tools, predictive placement, visual markers, CC0 background music, manual quick builds, and a fully rebindable keyboard/mouse controls HUD.
 // @match        *://moomoo.io/*
@@ -17,7 +17,9 @@
 // @run-at       document-start
 // @namespace    thisisirrelevandandidontknowwhygreasyforkneedsthisanyways
 // @license      MIT
-// @require      https://cdn.jsdelivr.net/gh/CoderGuy508/kitty-klient-cdn@efa564f0896cae56f45b684dedef237b05bff886/kitty-klient-core.user.js
+// @updateURL    https://raw.githubusercontent.com/CoderGuy508/kitty-klient-cdn/main/kitty-klient-loader.user.js
+// @downloadURL  https://raw.githubusercontent.com/CoderGuy508/kitty-klient-cdn/main/kitty-klient-loader.user.js
+// @require      https://cdn.jsdelivr.net/gh/CoderGuy508/kitty-klient-cdn@7dd4cc4ff21908412eae929ca68f6c0a6c4a131b/kitty-klient-core.user.js
 // ==/UserScript==
 
 void 0;
